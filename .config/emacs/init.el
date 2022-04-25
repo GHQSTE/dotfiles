816260cb514a316f4de75f0b679def63ba4cce91
Wasiqur Rahman <>
2022-04-25 22:08:56 +0600

    update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -52,9 +52,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c0f4b66aa26aa3fded1cbefe50184a08f5132756523b640f68f3e54fd5f584bd" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "1436985fac77baf06193993d88fa7d6b358ad7d600c1e52d12e64a2f07f07176" "db7f422324a763cfdea47abf0f931461d1493f2ecf8b42be87bbbbbabf287bfe" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "f5b6be56c9de9fd8bdd42e0c05fecb002dedb8f48a5f00e769370e4517dde0e8" "fc48cc3bb3c90f7761adf65858921ba3aedba1b223755b5924398c666e78af8b" default))
- '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
+ '(package-selected-packages
+   '(marginalia orderless savehist vertico doom-modeline all-the-icons doom-themes rainbow-delimiters rainbow-mode try blackout el-get hydra leaf-keywords leaf)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
