7b7892c9b4a6c8c5bf7fefa4f8d5ead1fc9da1fd
Wasiqur Rahman <>
2022-04-25 16:55:20 +0600

    update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -25,6 +25,9 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
+(leaf try
+  :ensure t)
+
 ;; This is the actual config file.
 ;; It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.config/emacs/config.org")
@@ -44,3 +47,15 @@
 (global-set-key (kbd "C-c r") 'config-reload)
 
 ;; init.el ends here.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
