a95fe5f34b97c62432d42dde6ba7f76a605d767f
Wasiqur Rahman <>
2022-11-16 05:44:19 +0600

    arst

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -10,7 +10,7 @@ local keymap = vim.api.nvim_set_keymap
 -- Map <leader> to space
 g.mapleader = ' '
 g.maplocalleader = ' '
-keymap("n", "<Space>", "", opts)
+keymap('n', '<Space>', '', opts)
 
 -- Modes
 --   normal_mode = 'n',
@@ -38,14 +38,14 @@ keymap('n', '<C-Right>', ':vertical resize -2<CR>', opts)
 keymap('n', '<leader>O', 'O<ESC>', opts)
 keymap('n', '<leader>o', 'o<ESC>', opts)
 
--- Move line up and down in NORMAL, INSERT and VISUAL modes
+-- Move line up and down in NORMAL, INSERT and V-LINE modes
 -- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
 keymap('n', '<A-j>', ':m .+1<CR>==', opts)
 keymap('n', '<A-k>', ':m .-2<CR>==', opts)
 keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', opts)
 keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', opts)
-keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
-keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
+keymap('x', '<A-j>', ":m '>+1<CR>gv=gv", opts)
+keymap('x', '<A-k>', ":m '<-2<CR>gv=gv", opts)
 
 -- navigate within insert mode
 keymap('i', '<C-h>', '<Left>', opts)
