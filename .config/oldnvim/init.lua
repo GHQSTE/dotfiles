2c137aa399c39e5339bfc9ad13fd3e420eaed8be
Wasiqur Rahman <>
2022-12-23 13:19:41 +0600

    arst

--- .config/oldnvim/init.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/lewis6991/impatient.nvim
-require('impatient')
-
--- general
-require "core.options"
-require "core.plugins"
-require "core.colorscheme"
-require "core.keymaps"
